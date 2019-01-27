526c33beb27abd94e91780aef0ee53201e4198d7
Salif <salif@salif-Satellite-L755>
2019-01-27 07:39:52 +0000

    first commit

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+<p>&copy; {{ date('Y') }} &middot; Laracarte by <a href="http://Salif.ci">Mon Journal web</a> 	-A Laracarte clone app. </p>
+<p><b> Projet test unitaire M2SIGL </b></p>
+  
+</footer>
